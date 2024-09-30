46207f0f630aa681d3fed26aa1ec0f91fa44dda9
codeREXus <147015708+codeREXus@users.noreply.github.com>
2024-09-30 23:47:35 +0530

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -14,7 +14,7 @@ class twosum {
        
     }
     
-    public static void main(String[] args)
+public static void main(String[] args)
     {
     	int nums[]={23,34,45,6,57,89};
         int tar=57;
