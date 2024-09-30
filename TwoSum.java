69b18bbe9df41ebae3d0f71acaa7fe77510190f7
codeREXus <147015708+codeREXus@users.noreply.github.com>
2024-09-30 23:46:51 +0530

    Create TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,23 @@
+
+class twosum {
+    public static void twoSum(int[] nums, int target) {
+        int i=0,j=0;
+        for(i=0;i<nums.length;i++){
+            for(j=1;j<nums.length;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    System.out.println("["+i+","+j+"]");
+                }
+            }
+        }
+       
+    }
+    
+    public static void main(String[] args)
+    {
+    	int nums[]={23,34,45,6,57,89};
+        int tar=57;
+        twoSum(nums,tar);
+    }
+}
