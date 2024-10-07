b209dba755bc3c0caf441ad03060923d4381c249
Anant srivastava <srivastavaanant007@gmail.com>
2024-10-07 23:47:55 +0530

    Add files via upload

---
+++ Binary Search/CeilingNumber.java
@@ -0,0 +1,41 @@
+package BinarySearch;
+
+public class CeilingNumber {
+	public static void main(String[] args) {
+		int [] arr= {12,23,34,44,53};
+		int target=54;
+		int ans=0;
+		ans=orderAgnosticBinarySearch(arr, target);
+		System.out.println("ceiling of the number is "+ans);
+	}
+	static int orderAgnosticBinarySearch(int[] arr , int target) {
+		int start=0;
+		int end=arr.length-1;
+		if(target>arr[end])
+			return -1;
+		boolean check=arr[start]>arr[end];
+			while(start<=end) {
+				int mid=0;
+				mid= start+(end-start)/2;
+				if(arr[mid]==target) {
+					return arr[mid];
+				}
+				if(check)
+				{					
+					if(arr[mid]>target) {
+						start=mid+1;
+					}
+					else
+						end=mid-1;
+				}
+				else {
+					}
+					if(arr[mid]<target) {
+						start=mid+1;
+					}
+					else
+						end=mid-1;
+				}
+		return arr[start];
+}
+}
---
+++ Binary Search/FloorNumber.java
@@ -0,0 +1,42 @@
+package BinarySearch;
+
+public class FloorNumber {
+	public static void main(String[] args) {
+		int [] arr= {12,23,34,44,53};
+		int target=53;
+		int ans=0;
+		ans=orderAgnosticBinarySearch(arr, target);
+		System.out.println("floor of the number is "+ans);
+	}
+	static int orderAgnosticBinarySearch(int[] arr , int target) {
+		int start=0;
+		int end=arr.length-1;
+		if(target<arr[start])
+			return -1;
+		boolean check=arr[start]>arr[end];
+			while(start<=end) {
+				int mid=0;
+				mid= start+(end-start)/2;
+				if(arr[mid]==target) {
+					return arr[mid];
+				}
+				if(check)
+				{					
+					if(arr[mid]>target) {
+						start=mid+1;
+					}
+					else
+						end=mid-1;
+				}
+				else {
+					}
+					if(arr[mid]<target) {
+						start=mid+1;
+					}
+					else
+						end=mid-1;
+				}
+		return arr[end];
+}
+
+}
