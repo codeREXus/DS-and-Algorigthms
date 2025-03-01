8fa8958be20cbbd8a5a4c297daff6c9d3b557bc0
Anant srivastava <srivastavaanant007@gmail.com>
2025-03-01 07:24:01 +0000

    Add main method for testing in Build Array from Permutation and rename Solution class to Solution3

--- Problems Leetcode/Build_Array_from_Permutation.java
+++ Problems Leetcode/Build_Array_from_Permutation.java
@@ -6,4 +6,12 @@ class Solution3{
         }
         return ans;
     }
+    public static void main(String[] args) {
+        int[] nums = {0,2,1,5,3,4};
+        Solution3 s = new Solution3();
+        int[] ans = s.buildArray(nums);
+        for(int i=0;i<ans.length;i++){
+            System.out.print(ans[i]+" ");
+        }
+    }
 }
